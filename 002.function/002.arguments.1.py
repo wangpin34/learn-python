14b54273d8dacbce15b745ca7bdd33a7808abe21
wangpin34 <270280522@qq.com>
2017-02-13 17:29:44 +0800

    Update function arguments demo

--- 002.function/002.arguments.1.py
+++ 002.function/002.arguments.1.py
@@ -1,8 +1,7 @@
 # python function def
 print '************** Function arguments Test Programs **************'
 
-print 'default arguments'
-
+# Type check
 def power_001(x):
 	if not isinstance(x,(int,float)):
 		raise TypeError('Bad Operand Type')
@@ -11,13 +10,13 @@ def power_001(x):
 
 print power_001(2)
 
-
+# Default arguments
 def power_002(x,n=2):
 	if not isinstance(x,(int,float)):
 		raise TypeError('Bad Operand Type')
 	else:
 		sum = 1;
-		while n>0:
+		while n > 0:
 			sum = x * sum
 			n = n - 1
 		return sum
@@ -26,29 +25,15 @@ print power_002(2)
 print power_002(2,8)
 
 
-def enroll_001(name, gender):
-    print 'name:', name
-    print 'gender:', gender
-
-enroll_001('Remind','Male')
-
-def enroll_002(name, gender,age=6,city='shanghai'):
-    print 'name:', name
-    print 'gender:', gender
-    print 'age:',age
-    print 'city:',city
-
-enroll_002('Remind','Male')
-enroll_002('Mona','female',4,'Gaoyou')
-enroll_002('Nita','female',city='Shangrao')
-
 def add_end(L=[]):
     L.append('END')
     return L
 
 print add_end()
 print add_end()
+print add_end()
 
+# Default argument must be a immutable object like Str, None
 def add_end_expected(L=None):
     if L is None:
         L = []
--- 002.function/004.arguments.3.py
+++ 002.function/004.arguments.3.py
@@ -4,15 +4,18 @@ print '************** Function arguments Test Programs **************'
 print 'keyword arguments'
 
 def person(name, age, **kw):
-    print 'name:', name, 'age:', age, 'other:', kw
+    print 'name:', name, 'age:', age
+    for name, value in kw.items():
+        print '{0}: {1}'.format(name, value)
 
 person('Remind',6)
 
 person('Remind',6,city='shanghai')
 
-kw = {'city':'shanghai','job':'Java'}
+namedParameters = {'city':'shanghai','job':'Java'}
 
-person('Remind',6,**kw)
+person('Remind',6, city = 'shanghai', job = 'Java')
+person('Remind',6, **namedParameters)
 
 
 raw_input()
